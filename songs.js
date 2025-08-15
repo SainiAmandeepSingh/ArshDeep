13e0f23b0be00e2963f8c14dcc2f1463ea38ae05
Singh AmanDeep Saini <121757220+SainiAmandeepSingh@users.noreply.github.com>
2025-08-15 17:02:23 +0200

    Add basic music website scaffold

---
+++ songs.js
@@ -0,0 +1,14 @@
+const songs = [
+  {
+    title: "Birthday Vibes",
+    releaseDate: "2024-12-01T00:00:00",
+    file: "songs/birthday-vibes.mp3",
+    lyrics: `Here comes the beat\nCelebrate the heat`
+  },
+  {
+    title: "Future Song",
+    releaseDate: "2024-12-10T00:00:00",
+    file: "",
+    lyrics: `Lyrics coming soon`
+  }
+];
